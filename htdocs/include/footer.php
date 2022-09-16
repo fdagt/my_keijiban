0c54f92943198cc0bc5766e064446846d76c4b91
fdagt <112229551+fdagt@users.noreply.github.com>
2022-09-16 21:49:17 +0900

    不要なリンクを削除

--- htdocs/include/footer.php
+++ htdocs/include/footer.php
@@ -1,10 +1,6 @@
 <footer>
     <ul>
 	<li><a href="/new-thread.php">新しいスレッド</a></li>
-	<li><a href="/report.php<?php 
-				if (isset($current_thread_id)) 
-				    echo '?id=', $current_thread_id;
-				?>">通報</a></li>
 	<li><a href="/admin/index.php">管理ページ</a></li>
     </ul>
 </footer>
